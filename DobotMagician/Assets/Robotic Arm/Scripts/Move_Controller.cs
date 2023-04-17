9823b3ec73912cbdddc2d28d7f9219e387729959
LabVR-Fime <ahmed.musulelf@uanl.edu.mx>
2023-04-16 18:34:04 -0600

    Versión2ya con UR

--- DobotMagician/Assets/Robotic Arm/Scripts/Move_Controller.cs
+++ DobotMagician/Assets/Robotic Arm/Scripts/Move_Controller.cs
@@ -2,58 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Move_Controller : MonoBehaviour
 {
-    public Slider baseSlider;
-    public Slider armSlider;
-    public Slider arm2Slider;
-    public Slider arm3Slider;
 
-    // slider value for base platform that goes from -1 to 1.
-    public float baseSliderValue = 0.0f;
-    // slider value for upper arm that goes from -1 to 1.
-    public float upperArmSliderValue = 0.0f;
+        public Slider baseSlider;
+        public Slider armSlider;
+        public Slider arm2Slider;
+        public Slider arm3Slider;
 
-    // slider value for upper arm2 that goes from -1 to 1.
-    public float upperArm2SliderValue = 0.0f;
+        // slider value for base platform that goes from -1 to 1.
+        public float baseSliderValue = 0.0f;
+        // slider value for upper arm that goes from -1 to 1.
+        public float upperArmSliderValue = 0.0f;
 
-    // slider value for upper arm3that goes from -1 to 1.
-    public float upperArm3SliderValue = 0.0f;
+        // slider value for upper arm2 that goes from -1 to 1.
+        public float upperArm2SliderValue = 0.0f;
 
-    // These slots are where you will plug in the appropriate arm parts into the inspector.
-    public Transform robotBase;
-    public Transform upperArm;
-    public Transform upperArm2;
-    public Transform upperArm3;
+        // slider value for upper arm3that goes from -1 to 1.
+        public float upperArm3SliderValue = 0.0f;
 
-    // Allow us to have numbers to adjust in the inspector the speed of each part's rotation.
-    public float baseTurnRate = 1.0f;
+        // These slots are where you will plug in the appropriate arm parts into the inspector.
+        public Transform robotBase;
+        public Transform upperArm;
+        public Transform upperArm2;
+        public Transform upperArm3;
 
-    private float baseZRot = 0.0f;
-    public float baseZRotMin = -90.0f;
-    public float baseZRotMax = 90.0f;
+        // Allow us to have numbers to adjust in the inspector the speed of each part's rotation.
+        public float baseTurnRate = 1.0f;
 
-    //Arm 1
-    public float upperArmTurnRate = 1.0f;
+        private float baseZRot = 0.0f;
+        public float baseZRotMin = -90.0f;
+        public float baseZRotMax = 90.0f;
 
-    private float upperArmZRot = -100f;
-    public float upperArmZRotMin = -160.0f;
-    public float upperArmZRotMax = -60.0f;
+        //Arm 1
+        public float upperArmTurnRate = 1.0f;
 
-    //Arm2
-    public float upperArm2TurnRate = 1.0f;
+        private float upperArmZRot = -100f;
+        public float upperArmZRotMin = -160.0f;
+        public float upperArmZRotMax = -60.0f;
 
-    private float upperArm2ZRot = 0f;
-    public float upperArm2ZRotMin = -20;
-    public float upperArm2ZRotMax = 80;
+        //Arm2
+        public float upperArm2TurnRate = 1.0f;
 
-    //Arm3
-    public float upperArm3TurnRate = 1.0f;
+        private float upperArm2ZRot = 0f;
+        public float upperArm2ZRotMin = -20;
+        public float upperArm2ZRotMax = 80;
 
-    private float upperArm3ZRot = 0f;
-    public float upperArm3ZRotMin = -110;
-    public float upperArm3ZRotMax = 10;
+        //Arm3
+        public float upperArm3TurnRate = 1.0f;
+
+        private float upperArm3ZRot = 0f;
+        public float upperArm3ZRotMin = -110;
+        public float upperArm3ZRotMax = 10;
+
 
     void Start()
     {
