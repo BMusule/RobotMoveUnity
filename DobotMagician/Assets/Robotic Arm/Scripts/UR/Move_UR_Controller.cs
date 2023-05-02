4334dc69792d8b1a9c57eb3ad809173d4fb9085a
LabVR-Fime <ahmed.musulelf@uanl.edu.mx>
2023-05-02 07:33:18 -0600

    02/05/23
    Versión escritorio con botones

--- DobotMagician/Assets/Robotic Arm/Scripts/UR/Move_UR_Controller.cs
+++ DobotMagician/Assets/Robotic Arm/Scripts/UR/Move_UR_Controller.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using System;
+using UnityEngine.UI; 
 
 public class Move_UR_Controller : MonoBehaviour
 {
